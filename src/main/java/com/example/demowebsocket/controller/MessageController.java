c4eca21260d99cb6f70169ae60d3234b5c12a4c4
thangpd <thangpd@unikom.vn>
2022-07-19 15:15:01 +0700

    done

--- src/main/java/com/example/demowebsocket/controller/MessageController.java
+++ src/main/java/com/example/demowebsocket/controller/MessageController.java
@@ -3,10 +3,8 @@ package com.example.demowebsocket.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
-@CrossOrigin("*")
 public class MessageController {
 
     @MessageMapping("/hello") // Tưởng tượng giống requestMapping, value là giá trị của endpoint mà chúng ta đã cấu hình trong class WebSocketConfiguration
